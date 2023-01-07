063e3515b2547642224ad0b8bf3a3fc9479139b6
Yudhriz <riskyyudha678@gmail.com>
2023-01-07 08:48:59 +0700

    update by Yudha

--- assest/js/script.js
+++ assest/js/script.js
@@ -4,3 +4,23 @@ searchButton.addEventListener('click', () => {
     const inputValue = searchInput.value;
     alert(inputValue);
 });
+
+function check_jika_login() {
+
+
+    if (login == false) {
+        window.location = "login/login.html";
+        login = true;
+    }
+
+}
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username == "admin" && password == "admin") {
+        window.alert("login sukses");
+        window.location.href = "../";
+
+    }
+};
