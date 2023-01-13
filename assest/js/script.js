2dbcf88c6b93ba353baf37bc429705ff0560d40e
Yudhriz <riskyyudha678@gmail.com>
2023-01-13 21:04:58 +0700

    update assest css js, login, register,by Yudha

--- assest/js/script.js
+++ assest/js/script.js
@@ -4,13 +4,3 @@ searchButton.addEventListener('click', () => {
     const inputValue = searchInput.value;
     alert(inputValue);
 });
-
-function login() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    if (username == "admin" && password == "admin") {
-        window.alert("login sukses");
-        window.location.href = "../admin/admin.html";
-
-    }
-};
